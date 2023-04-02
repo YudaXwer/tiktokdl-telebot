bae8a709495e49c7047c40c10505be29a5f3e3e0
Yuda Xwer <81358285+YudaXwer@users.noreply.github.com>
2023-04-02 17:04:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 module.exports.admin_id = [{ id: 123456789, username: "username" }];
-module.exports.bot_token = "Your Bot Token";
+module.exports.bot_token = "5999431093:AAEuVF0lCGXAGty1ohkh6oQ4m63b_FNJoPk";
